55e790ff3ed29451b960cdce4aab29ae217b07ec
Knnapierala <knnapierala@wp.pl>
2022-06-02 01:03:05 +0200

    added matching logic

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
 const cards = document.querySelectorAll('.game-card');
 
+let hasFlippedCard = false;
+let firstCard, secondCard
+
 function swichCard() {
   this.classList.toggle('swiched')
-}
 
+  if (!hasFlippedCard) {
+    //first card
+    hasFlippedCard = true;
+    firstCard = this;
+  } else {
+    //second card
+    hasFlippedCard = false;
+    secondCard = this;
+  }
+    if (firstCard.dataset.shape === secondCard.dataset.shape ) {
+      firstCard.removeEventListener('click', swichCard)
+      secondCard.removeEventListener('click', swichCard)
+    } else {
+      setTimeout(() => { 
+      firstCard.classList.remove('switched');
+      secondCard.classList.remove('switched');
+    }, 1500);
+    }
+
+}
 cards.forEach(card => card.addEventListener('click', swichCard))
 
